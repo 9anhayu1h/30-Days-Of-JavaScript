c2953ac04005e64445bfe950ab84724b8dc6a47a
9anhayu1h <oeun.panhayuth.1521@rupp.edu.kh>
2022-11-18 15:03:23 +0700

    Exercise

--- 03_Day_Booleans_operators_date/03_day_starter/3_Exercise/lvl_2.js
+++ 03_Day_Booleans_operators_date/03_day_starter/3_Exercise/lvl_2.js
@@ -1,3 +1,4 @@
+const prompt=require('prompt-sync')();
 //1
 let base = prompt('Enter base:');
 let height = prompt('Enter height:');
@@ -22,5 +23,47 @@ let r = prompt('radius: ');
 let areaCircle = Math.PI * r * r
 console.log(`area of the circle = ${areaCircle}`);
 //5
-let x  = prompt('Enter value of x: ');
+let x  = 1
 let y = 2 * x + 2
+console.log(x, y);
+//6
+let p1 = [2, 2]
+let p2 = [6, 10]
+let m = (p2[1]-p1[1])/(p2[0]-p1[0])
+console.log(m);
+//8
+let x8 = -3
+let y8 = x ** 2 + 6 * x + 9
+console.log(x, y);
+//9
+let Totalhour = parseFloat(prompt('Enter hours: '));
+let PaymentRate = parseFloat(prompt('Enter rate per hour: '));
+let WeeklyEarning = Totalhour * PaymentRate
+console.log(`Enter hours: ${Totalhour}`, `\nEnter rate per hour: ${PaymentRate}`, `\nYour weekly earning is: ${WeeklyEarning}`);
+//10
+let MyName = 'Panhayuth'
+MyName.length > 7
+    ? console.log('My name is long')
+    : console.log('My name is short')
+//11
+let LastName = 'Oeun'
+MyName > LastName
+    ? console.log(`Your first name, ${MyName} is longer than your family name, ${LastName}`)
+    : console.log(`Your first name, ${MyName} is shorter than your family name, ${LastName}`)
+//12
+let myAge = 250
+let yourAge = 25
+myAge > yourAge
+    ? console.log(`I am ${myAge - yourAge} years older than you.`)
+    : console.log(`You are ${yourAge - myAge} years older than me.`)
+//13
+let UserBirthYear = parseFloat(prompt('Enter birth year: '))
+let Present = new Date()
+let UserAge = Present.getFullYear() - UserBirthYear
+UserAge > 18
+    ? console.log(`You are ${UserAge}. You are old enough to drive`)
+    : console.log(`You are ${UserAge}. You will be allowed to drive after ${18 - UserAge} years.`)
+//14
+let NumOfYear = parseFloat(prompt('Enter number of years you live: '))
+let LiveInSec = NumOfYear * 31536000
+console.log()
