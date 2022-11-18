91245526e455d4076247aaec754741dfabdf7369
9anhayu1h <oeun.panhayuth.1521@rupp.edu.kh>
2022-11-18 15:03:35 +0700

    Exercise

---
+++ 03_Day_Booleans_operators_date/03_day_starter/3_Exercise/9.js
@@ -0,0 +1,20 @@
+let x = -3
+let y = x ** 2 + 6 * x + 9
+console.log(x, y);
+//10
+let MyName = 'Panhayuth'
+MyName.length > 7
+    ? console.log('My name is long')
+    : console.log('My name is short')
+//11
+let LastName = 'Oeun'
+MyName > LastName
+    ? console.log(`Your first name, ${MyName} is longer than your family name, ${LastName}`)
+    : console.log(`Your first name, ${MyName} is shorter than your family name, ${LastName}`)
+//12
+let myAge = 250
+let yourAge = 25
+myAge > yourAge
+    ? console.log(`I am ${myAge - yourAge} years older than you.`)
+    : console.log(`You are ${yourAge - myAge} years older than me.`)
+//13
