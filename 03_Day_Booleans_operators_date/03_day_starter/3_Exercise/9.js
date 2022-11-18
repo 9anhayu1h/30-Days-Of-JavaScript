930755dfcfb2f7ebb6a29b39e8561d359693caea
9anhayu1h <oeun.panhayuth.1521@rupp.edu.kh>
2022-11-18 15:30:25 +0700

    Exercise

--- 03_Day_Booleans_operators_date/03_day_starter/3_Exercise/9.js
+++ 03_Day_Booleans_operators_date/03_day_starter/3_Exercise/9.js
@@ -17,4 +17,18 @@ let yourAge = 25
 myAge > yourAge
     ? console.log(`I am ${myAge - yourAge} years older than you.`)
     : console.log(`You are ${yourAge - myAge} years older than me.`)
-//13
+//15
+const now = new Date()
+const year = now.getFullYear() 
+const month = now.getMonth() + 1 
+const date = now.getDate() 
+const hours = now.getHours() 
+const minutes = now.getMinutes()
+
+console.log(`${year}-${month}-${date} ${hours}:${minutes}`)
+console.log(`${date}-${month}-${year} ${hours}:${minutes}`)
+console.log(`${date}/${month}/${year} ${hours}:${minutes}`)
+//16
+hours < 10 && minutes < 10
+    ? console.log(`${year}-${month}-${date} 0${hours}:0${minutes}`)
+    : console.log(`${year}-${month}-${date} ${hours}:${minutes}`)
